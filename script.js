55a0116d8f8e5a94a9651bf9dcbbeba9030471db
Duwelzdorian <duwelzdorian@gmail.com>
2019-05-15 12:15:21 +0200

    exo4

---
+++ script.js
@@ -0,0 +1,6 @@
+function resetForm() {
+  document.getElementByid('lastname').value = '';
+  document.getElementByid('firstname').value = '';
+  document.getElementByid('city').value = '';
+  alert('Merci de votre participation');
+}
